ac40d292b3cd70a27c66e40077a8e4e14e582124
Phillip Eckert <phillip.eckert@ezag.de>
2023-09-10 22:05:09 -0400

    new start page

--- app/main.py
+++ app/main.py
@@ -33,6 +33,6 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to the FastApi Project!!!!!!"}
+    return {"message": "Welcome to the FastApi Project from peckert!"}
 
 
